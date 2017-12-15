e47682e0489e0ecf89742f8406c7e9deec2975a8
Sylvie Dagoret-Campagne <dagoret@lal.in2p3.fr>
2017-12-15 13:13:58 +0100

    update to read HD Calspec

--- DESCDec2017/pysynphotsed/libpysynphotsed.py
+++ DESCDec2017/pysynphotsed/libpysynphotsed.py
@@ -7,10 +7,12 @@ Created on Thu Dec 14 14:37:29 2017
 """
 
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 import os
+import re
+
+from astropy.io import fits
 
 # to enlarge the sizes
 params = {'legend.fontsize': 'x-large',
@@ -21,15 +23,22 @@ params = {'legend.fontsize': 'x-large',
          'ytick.labelsize':'x-large'}
 plt.rcParams.update(params)
 
-top_pysynphot_data_dir=os.environ['PYSYN_CDBS']
 import pysynphot as S
 S.primary_area=6*1e4
 S.binning=10.
 
 
+
+# directories
+top_pysynphot_data_dir=os.environ['PYSYN_CDBS']
+dir_star='calspec'
+dir_nostar='grid'
+dir_submodels=['ags','bpgs','extinction','jacobi','phoenix','bc95','bz77','galactic','k93models','pickles','bkmodels','ck04models','gunnstryker','kc96']
+
+
+#star models
 sed_colors_mpl=['c','b', 'g', 'r', 'm', 'y', 'k', 'w']
 TypeStar=["O","B","A","F","G","K"]
-
 
 
 TypeStar_to_Temperature = {"O":25000., "B":10000., "A":7500.,"F":6000.,"G":5000.,"K":3500 }
@@ -37,6 +46,8 @@ TypeStar_to_color= {"O":'c', "B":'b', "A":'g',"F":'r',"G":'m',"K":"k" }
 TypeStar_to_number= {"O":0, "B":1, "A":2,"F":3,"G":4,"K":5 }
 Set_Log_g=[0.,1.,2.,3.,4.,5.]
 Set_Log_Z=[-2.5,-2.,-1.5,-1.,-.5,0.,0.2,0.5]
+
+
 
 
 #-------------------------------------------------------------------------------------------
@@ -123,7 +134,7 @@ def get_all_phoenixmodels():
         all_sed.append(all_sub_sed)
     return all_sed
 #---------------------------------------------------------------------------------------
-def plot_allsed(all_sed,thetitle,figfilename):
+def plot_allsed_starmodels(all_sed,thetitle,figfilename,yscale='lin'):
     
     NBSEDCOLORS=len(all_sed)
     
@@ -134,9 +145,15 @@ def plot_allsed(all_sed,thetitle,figfilename):
         for sed in sed_coll:
             
             if index==0:
-                plt.semilogy(sed.wave,sed.flux,sed_colors_mpl[icol],label=TypeStar[icol])
+                if yscale=='log' :
+                    plt.semilogy(sed.wave,sed.flux,sed_colors_mpl[icol],label=TypeStar[icol])
+                else:
+                    plt.plot(sed.wave,sed.flux,sed_colors_mpl[icol],label=TypeStar[icol])
             else:
-                plt.semilogy(sed.wave,sed.flux,sed_colors_mpl[icol])
+                if yscale=='log' :
+                    plt.semilogy(sed.wave,sed.flux,sed_colors_mpl[icol])
+                else:
+                    plt.plot(sed.wave,sed.flux,sed_colors_mpl[icol])
 
             index+=1
     plt.xlim(0, 11000)
@@ -150,13 +167,104 @@ def plot_allsed(all_sed,thetitle,figfilename):
     
 #-----------------------------------------------------------------------------------------------
 
+def get_all_calspec_hd():
+    
+    SEDfile_dir=os.path.join(top_pysynphot_data_dir,'calspec')
+    filelist=os.listdir(SEDfile_dir) 
+    fits_files = [f for f in os.listdir(SEDfile_dir) if f.endswith('.fits')]
+    
+    # extract header and filename
+    star_header = []
+    star_file_calspec = []   
+    for filename in filelist:       
+        index=0
+        if re.search('fits',filename) and re.search('hd',filename) and re.search('stis',filename):  #example of filename filter
+            index+=1
+            fullfilename = os.path.join(SEDfile_dir,filename)
+            hdr = fits.getheader(fullfilename)
+            star_header.append(hdr)
+            star_file_calspec.append(filename)
+            
+    # extract starname    
+    star_names = []
+    index=0
+    for hdr in star_header: 
+#    print index
+        if index!=433:
+            star_name=star_header[index]['TARGETID']
+            star_names.append(star_name)
+            index+=1
+        else:
+            print '>>>>>> skip file # ',index, 'BAD HEADER'
+            print '>>>>>> filename = ', filelist[index]
+            print hdr
+            index+=1
+        
+    # sort filename    
+    star_names_sorted=sorted(star_names,key=star_names.count,reverse=True)
+    star_names_sorted_upper = map(lambda s: s.upper(), star_names_sorted)
+    star_names_set=set(star_names_sorted_upper)
+
+    # build dictionnary of filename (several files per star name)
+    StarDict= {}
+    for star in star_names_set:
+        print star,': \n'
+        star_set_of_file= []
+        tag_upper='^'+star.upper()+'*'
+        tag_lower='^'+star.lower()+'*'
+        
+        for thefile in fits_files:
+            if re.search(tag_upper,thefile) or re.search(tag_lower,thefile):         
+                star_set_of_file.append(thefile)
+        #StarDict[star]=sorted(star_set_of_file,key=star_names.count,reverse=True)
+        StarDict[star]=sorted(star_set_of_file,key=star_names.count)
+        print StarDict[star] ,'\n'
+      
+    # SED 
+    all_sed=[]    
+    for keystar in StarDict:
+        the_files=StarDict[keystar]
+        if(len(the_files))>0 and keystar != 'SUN_REFERENCE':
+        #print sorted(the_files,reverse=True)
+        
+            selected_file=the_files[0]
+            selected_fullfile=os.path.join(SEDfile_dir,selected_file)
+     
+            sed=S.FileSpectrum(selected_fullfile)
+            if(max(sed.flux)>0): # remove empty fluxes because of bad parameters
+                all_sed.append(sed)
+    return all_sed
+        
+#-----------------------------------------------------------------------------------------------
+   
+def plot_allsed(all_sed,thetitle,figfilename,yscale='lin'):
+      
+    for sed in all_sed:     
+        if yscale=='log':
+            plt.semilogy(sed.wave,sed.flux)
+        else:
+            plt.plot(sed.wave,sed.flux)
+
+    plt.xlim(0, 11000)
+    plt.xlabel(sed.waveunits)
+    plt.ylabel(sed.fluxunits)
+    plt.grid(True)
+    plt.title(thetitle)
+    plt.savefig(figfilename)
+    
+
+
 #------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     print 'hello'
 
-    all_sed=get_all_ck04models()
+    #all_sed=get_all_ck04models()
     
-    plot_allsed(all_sed,"ck04models","ck04models.png")
+    #plot_allsed_starmodels(all_sed,"ck04models","ck04models.png")
+    
+    all_sed=get_all_calspec_hd()
+    plot_allsed(all_sed,'SED of CALSPEC stars','calspec_hd_lin.png',yscale='lin')
+    plot_allsed(all_sed,'SED of CALSPEC stars','calspec_hd_log.png',yscale='log')
 
     
     
